3ec15595ebb1e8a873349a1ac98f13208b612629
romgharti <romgharti@gmail.com>
2024-11-20 19:14:32 +0545

    sm6150-common: Build android.hardware.keymaster@4.0.vendor
    Signed-off-by: romgharti <romgharti@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -143,6 +143,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0 \
     android.hardware.gatekeeper@1.0.vendor \
+    android.hardware.keymaster@4.0.vendor \
     android.hardware.keymaster@4.1 \
     android.hardware.keymaster@4.1.vendor
 
